e204bef65d00dd20d2d0b590c303cdfc25ab566c
Isobel Beasley <isobel.beasley@ucsf.edu>
2024-11-08 20:44:36 -0800

    Update tsinfer pipeline testing

--- code/tsinfer_practice1.py
+++ code/tsinfer_practice1.py
@@ -3,89 +3,107 @@ import tsinfer
 import time as time
 import numpy as np
 
+import warnings
+warnings.simplefilter(action='ignore', 
+                      category=FutureWarning
+                      )
+
 
 ################# Argparse arguments ######################
+
 import argparse
 parser = argparse.ArgumentParser()
 
 # Required inputs 
 req_group = parser.add_argument_group(title='REQUIRED INPUT')
-req_group.add_argument('-vcf', '-v', help='VCF/BCF file', required=True)
-req_group.add_argument('-out', '-o', help='Output sampleData file', required=True)
+req_group.add_argument('-vcf', '-v', 
+                       help='VCF/BCF file', 
+                       required=True
+                       )
+req_group.add_argument('-out', '-o', 
+                       help='Output sampleData file', 
+                       required=True
+                       )
 
 # Optional inputs
 inp_group = parser.add_argument_group(title='OPTIONAL INPUT')
-inp_group.add_argument('-pop_assign', help = 'Text file assigning samples to populations'
-                        'Otherwise, all samples are assumed to be from the same population',
-                        default='undefined',
-                        required=False)
-inp_group.add_argument('-n_cpus', '-n', help = "Number of avaliable CPUs", required=False)
+inp_group.add_argument('-pop_assign', 
+                       help = 'Text file assigning samples to populations'
+                       'Otherwise, all samples are assumed to be from the same population',
+                       default='undefined',
+                       required=False
+                       )
+inp_group.add_argument('-n_cpus', '-n', 
+                       help = "Number of avaliable CPUs", 
+                       required=False
+                       )
 
 args = parser.parse_args()
 
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ########## Functions ################## 
 
-def add_diploid_populations(pop_assign, samples): 
-     """
-     Read in the population assignments, and set these up with the 
-     """
+def add_diploid_populations(pop_assign, samplesData): 
+    """
+    Read in the population assignments, and set these up with the 
+    """
 
-     if pop_assign == "undefined":
-       
-       samples.add_population(metadata={"name": pop_assign})
-       
-     else:
-       
-       # pop_assign = np.genfromtxt(pop_assign, 
-       #                            dtype=[('sample', 'U10'), ('population', 'U10')], 
-       #                            delimiter=' ')
-                                  
-       all_populations = np.unique(pop_assign['population'])
-       
-       for population in all_populations: 
+    if pop_assign == "undefined":
+      
+      samplesData.add_population(metadata={"name": pop_assign})
+
+    else:
+      
+      pop_assign = np.loadtxt(pop_assign, 
+                              dtype='U10', 
+                              delimiter=','
+                              )
+      
+      all_populations = np.unique(pop_assign[:, 1])
+
+      for population in all_populations:
+        
+        samplesData.add_population(metadata={"name": population})
          
-           samples.add_population(metadata={"name": population})
-         
        
 
-
-
-def add_diploid_individuals(vcf, pop_assign, samples):
+def add_diploid_individuals(pop_assign, vcf, samplesData):
     """
     Read in the samples from the vcf, and add them to the samples object
     """
     
     if pop_assign == "undefined":
       
-       for individual in vcf.samples:
+       for individual in vcf.samplesData:
          
-            samples.add_individual(ploidy = 2, population = 0, metadata={"name": individual})
+            samplesData.add_individual(ploidy = 2, 
+                                   population = 0, 
+                                   metadata={"name": individual}
+                                   )
     
-    else: 
+    else:
       
-      # pop_assign = np.genfromtxt(pop_assign, 
-      #                            dtype=[('sample', 'U10'), ('population', 'U10')], 
-      #                            delimiter=' ')
-                                 
-      all_populations = np.unique(pop_assign['population'])
-      
+      pop_assign = np.loadtxt(pop_assign, 
+                              dtype='U10', 
+                              delimiter=','
+                              )
+          
+      # getting mapping between sample id and population id                            
+      all_populations = np.unique(pop_assign[:, 1])
       population_to_num = {pop: i for i, pop in enumerate(all_populations)}
       
-      for individual_num in pop_assign.shape[0]:
+      for individual_num in range(pop_assign.shape[0]):
         
-          individual_pop = pop_assign['population'][individual_num]
+          individual_pop = pop_assign[individual_num, 1]
            
-          sample.add_individual(ploidy = 2, 
-                                population = population_to_num.get(individual_pop),
-                                metadata = {"name": individual_pop}
-                                )
+          samplesData.add_individual(ploidy = 2, 
+                                     population = population_to_num.get(individual_pop),
+                                     metadata = {"name": individual_pop}
+                                     )
 
     
 
-def add_diploid_sites(vcf, samples):
+def add_diploid_sites(vcf, samplesData):
     """
     Read the sites in the vcf and add them to the samples object, reordering the
     alleles to put the ancestral allele first, if it is available.
@@ -125,51 +143,50 @@ def add_diploid_sites(vcf, samples):
             for row in variant.genotypes
             for old_index in row[0:2]
         ]
-        samples.add_site(pos, genotypes=genotypes, alleles=ordered_alleles)
+        samplesData.add_site(pos, genotypes=genotypes, alleles=ordered_alleles)
 
 
-def chromosome_length(vcf):
-    assert len(vcf.seqlens) == 1
-    return vcf.seqlens[0]
+# def chromosome_length(vcf):
+#     assert len(vcf.seqlens) == 1
+#     return vcf.seqlens[0]
 
-###### Build sampleData file: ##########
+######### Build sampleData file: #############
 
 start_time = time.perf_counter()
 vcf = cyvcf2.VCF(args.vcf)
 
-#samples = vcf.samples
+#samples = vcf.samplesData
 
-if pop_assign != "undefined":
-  
-   
-   pop_assign = np.genfromtxt(pop_assign, 
-                              dtype=[('sample', 'U10'), ('population', 'U10')], 
-                              delimiter=' ')
-                                  
+# Set up population assignments for sampleData file: 
+pop_assign = args.pop_assign
 
 with tsinfer.SampleData(
     path=args.out
-) as samples:
-    add_diploid_populations(args.pop_assign, samples)
-    add_diploid_individuals(vcf, samples)
-    add_diploid_sites(vcf, samples)
+) as samplesData:
+    add_diploid_populations(pop_assign, samplesData)
+    add_diploid_individuals(pop_assign, vcf, samplesData)
+    add_diploid_sites(vcf, samplesData)
 
 print(
-    "\n \n Sample file created for {} samples: ".format(samples.num_samples)
-    + "\n {} populations".format(samples.num_populations)
-#    + "{})".samples.populations_metadata_schema
-    + "\n {} individuals ".format(samples.num_individuals)
-    + "\n {} variable sites.".format(samples.num_sites),
+    "\n \n Sample file created for {} samples: ".format(samplesData.num_samples)
+    + "\n {} populations".format(samplesData.num_populations)
+#    + "{})".samplesData.populations_metadata_schema
+    + "\n {} individuals ".format(samplesData.num_individuals)
+    + "\n {} variable sites.".format(samplesData.num_sites),
     flush=True,
 )
 
 end_time = time.perf_counter()
 print(f"\n Making the SampleData file took {end_time - start_time:.4f} seconds")
 
+
+############## Infering the ARG #######################
+
+
 # Do the inference
 start_time = time.perf_counter()
 
-ts = tsinfer.infer(samples)
+ts = tsinfer.infer(samplesData)
 print(
     "\n \n Inferred tree sequence: {} trees over {} Mb ({} edges)".format(
         ts.num_trees, ts.sequence_length / 1e6, ts.num_edges
