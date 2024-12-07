be05efed19ff23c3a99c5eaa6456b12e9073e126
Isobel Beasley <isobel.beasley@ucsf.edu>
2024-12-06 16:46:34 -0800

    Use complexheatmap to plot gnn matrix

--- code/plot_gnn.R
+++ code/plot_gnn.R
@@ -1,13 +1,9 @@
 
 mat = read.csv("matrix.csv", header = F)
+mat = as.matrix(mat)
 
 library(ggplot2)
 library(viridis)
 library(reshape2)
 
-XTidy = melt(mat, value.name="val", varnames = c("x", "y") )
-
-ggplot(XTidy, aes( x, y ) ) + 
-  geom_tile( aes( fill = val ) ) + 
-  geom_text( aes( label = val ) ) + 
-  scale_fill_viridis(name = "GNN", option = "D")
+plot =    ComplexHeatmap::Heatmap(mat = mat)
